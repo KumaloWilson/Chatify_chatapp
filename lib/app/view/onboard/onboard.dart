10dda56d75fe2b7327cbe8d189d8e40fc3551eb6
premnadh <premnathbr123@gmail.com>
2024-04-09 15:54:00 +0530

    done

--- lib/app/view/onboard/onboard.dart
+++ lib/app/view/onboard/onboard.dart
@@ -2,6 +2,7 @@
 
 import 'package:chat_app/app/view/root/root.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intro_screen_onboarding_flutter/introduction.dart';
 import 'package:intro_screen_onboarding_flutter/introscreenonboarding.dart';
 
@@ -10,23 +11,32 @@ class OnBoardScreen extends StatelessWidget {
     Introduction(
       imageUrl: 'assets/images/chatapp.gif',
       title: "Chat with Emojis and GIFs!",
-      titleTextStyle: const TextStyle(
-        fontSize: 16, // Adjust font size
+      titleTextStyle: GoogleFonts.poppins(
+        fontSize: 16,
+      ),
+      subTitleTextStyle: GoogleFonts.poppins(
+        fontSize: 18,
       ),
       subTitle: "Ping! Your friends are waiting. Keep chatting.",
     ),
     Introduction(
       imageUrl: 'assets/images/plane-unscreen.gif',
       title: "Connect and Spread Good Vibes!",
-      titleTextStyle: const TextStyle(
-        fontSize: 16, // Adjust font size
+      titleTextStyle: GoogleFonts.poppins(
+        fontSize: 16,
+      ),
+      subTitleTextStyle: GoogleFonts.poppins(
+        fontSize: 18,
       ),
       subTitle: "Tap, type, talk! Let the chatting adventures begin.",
     ),
     Introduction(
       imageUrl: 'assets/images/chat.gif',
-      titleTextStyle: const TextStyle(
-        fontSize: 16, // Adjust font size
+      titleTextStyle: GoogleFonts.poppins(
+        fontSize: 16,
+      ),
+      subTitleTextStyle: GoogleFonts.poppins(
+        fontSize: 18,
       ),
       title: "Welcome to EchoChat!",
       subTitle: "Your secrets are safe with us. EchoChat - where privacy meets",
@@ -41,7 +51,9 @@ class OnBoardScreen extends StatelessWidget {
       body: IntroScreenOnboarding(
         backgroudColor: Colors.white,
         introductionList: list,
-        skipTextStyle: const TextStyle(fontSize: 15),
+        skipTextStyle: GoogleFonts.poppins(
+          fontSize: 14,
+        ),
         onTapSkipButton: () {
           Navigator.of(context).pushReplacement(MaterialPageRoute(
             builder: (context) => const AuthenticationScreen(),
